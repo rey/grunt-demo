fdbd4ef37264795d6aa325997b1194960a41ea7d
Rey Dhuny <rey@me.com>
2013-12-19 11:09:37 +0000

    work in progress -- breaking change

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,33 +1,18 @@
 module.exports = function(grunt) {
 
-  // load all grunt tasks. ALL OF THEM
-  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
-
+  // initialise le config
   grunt.initConfig({
-
-    watch: {
-      options: {
-        livereload: true,
-      },
-      css: {
-        files: ['assets/stylesheets/sass/**/*.sass'],
-        tasks: ['compass']
-      }
-    },
-
-    compass: {
-      dist: {
-        options: {
-          // specify the location of the Compass configuration file explicitly
-          config: 'config.rb',
-          // allows compass to overwrite existing files
-          force: true
-        }
-      }
-    }
-
+    pkg: require('./package.json'),
   });
 
-  grunt.registerTask('default', ['watch']);
+  // load per task config from separate files
+  grunt.loadTasks('grunt');
 
-}
+  // register alias tasks
+  grunt.registerTask('dev', 'Tasks for development, son',
+    ['watch']
+  );
+
+  grunt.registerTask('default', ['dev']);
+
+};
