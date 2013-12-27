659ab557891ed1537428995f04715501f9050a3f
Rey Dhuny <rey@me.com>
2013-12-27 11:41:18 +0000

    make livereload options block generic for all watch tasks

--- grunt/grunt-contrib-watch.js
+++ grunt/grunt-contrib-watch.js
@@ -1,14 +1,14 @@
 module.exports = function(grunt) {
 
   grunt.config('watch', {
+    options: {
+      livereload: {
+        port: 1337,
+      },
+    },
     compass: {
       files: ['<%= compass.dev.src %>'],
       tasks: ['compass:dev'],
-      options: {
-        livereload: {
-          port: 1337,
-        },
-      },
     },
     jshint: {
       files: ['<%= jshint.app.src %>'],
